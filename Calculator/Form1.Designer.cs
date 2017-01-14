eeee7d859404913687f65378924d681ec813b006
David Wartenbe <thedavinatorw@gmail.com>
2017-01-13 21:41:02 -0600

    First commit

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbDisplay = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbDisplay
+            // 
+            this.tbDisplay.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbDisplay.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tbDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDisplay.Location = new System.Drawing.Point(0, 0);
+            this.tbDisplay.Multiline = true;
+            this.tbDisplay.Name = "tbDisplay";
+            this.tbDisplay.Size = new System.Drawing.Size(443, 25);
+            this.tbDisplay.TabIndex = 0;
+            this.tbDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnClear
+            // 
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(13, 32);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnEquals
+            // 
+            this.btnEquals.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnEquals.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEquals.Location = new System.Drawing.Point(356, 31);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(75, 23);
+            this.btnEquals.TabIndex = 3;
+            this.btnEquals.Text = "=";
+            this.btnEquals.UseVisualStyleBackColor = true;
+            this.btnEquals.Click += new System.EventHandler(this.btnEquals_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Enabled = false;
+            this.tbResult.Location = new System.Drawing.Point(94, 34);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(256, 20);
+            this.tbResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 70);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.tbDisplay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbDisplay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.TextBox tbResult;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    public enum Operator { PLUS, MINUS, DIVIDE, MULTIPLY };
+
+    public partial class Form1 : Form
+    {
+        double total = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Button Listeners
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            total = 0;
+            tbDisplay.Clear();
+            tbDisplay.Focus();
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            //tbDisplay.Clear();
+            //tbDisplay.Text = total.ToString();
+            //calcDisplay(tbDisplay.Text);
+            perfomCalculation(calcDisplay(tbDisplay.Text), tbDisplay.Text);
+            tbDisplay.Focus();
+        }
+        #endregion
+
+        #region Calculation Methods
+        /*
+        *Find where the operations are in the string, and which operation they are, store them in a List 
+        */
+        private List<OperationPoint> calcDisplay(String str)
+        {
+            //read numbers, find +-/*, calculate, display
+            List<OperationPoint> add = new List<OperationPoint>();
+            List<OperationPoint> minus = new List<OperationPoint>();
+            List<OperationPoint> divide = new List<OperationPoint>();
+            List<OperationPoint> multiply = new List<OperationPoint>();
+            List<OperationPoint> operatorLocation = new List<OperationPoint>();
+
+            //read string and assign location to thier list
+            for (int x = 0; x < str.Length; x++)
+            {
+                //find operators
+                switch (str[x])
+                {
+                    case '+':
+                        add.Add(new OperationPoint(x, Operator.PLUS));
+                        break;
+                    case '-':
+                        minus.Add(new OperationPoint(x, Operator.MINUS));
+                        break;
+                    case '*':
+                        multiply.Add(new OperationPoint(x, Operator.MULTIPLY));
+                        break;
+                    case '/':
+                        divide.Add(new OperationPoint(x, Operator.DIVIDE));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            //add all the location to one list
+            for (int x = 0; x < 4; x++)
+            {
+                switch (x)
+                {
+                    case 0:
+                        operatorLocation.AddRange(add);
+                        break;
+                    case 1:
+                        operatorLocation.AddRange(minus);
+                        break;
+                    case 2:
+                        operatorLocation.AddRange(divide);
+                        break;
+                    case 3:
+                        operatorLocation.AddRange(multiply);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            //sort the list
+            operatorLocation.Sort((x, y) => x.getPoint().CompareTo(y.getPoint()));
+            //display list to console
+            for (int x = 0; x < operatorLocation.Count; x++)
+            {
+                Console.WriteLine("Operation " + operatorLocation[x].getOperation() + " at point  " + operatorLocation[x].getPoint());
+            }
+            return operatorLocation;
+        }
+
+        /*
+         * Main calculation method. Use list of operations and their locations to parse the string,
+         * do the operation and store the result in total. This method finds the numbers within the string,
+         * does the operation between the numbers, store the result then continue along with the next number
+         * */
+        public void perfomCalculation(List<OperationPoint> op, String str)
+        {
+            int calcToPerform = op.Count;
+            double numberHoldOne = 0, numberHoldTwo = 0;
+            Boolean firstCalc = true;
+
+            //the number of operation are the number of objects in op
+            for (int x=0; x < calcToPerform; x++)
+            {
+                Console.WriteLine("Operation {0}", x+1);
+                if (firstCalc)
+                {
+                    numberHoldOne = double.Parse(str.Substring(0, op[x].getPoint()));
+                    firstCalc = false;
+                    Console.WriteLine("Got first number " + numberHoldOne.ToString());
+                }
+                else
+                {
+                    numberHoldOne = total;
+                    Console.WriteLine("Got first number " + numberHoldOne.ToString());
+                }
+
+                if (op.Count > (x + 1))//there are more operation to do
+                {
+                    /*
+                    * SubString arguments:
+                    * First = Grab index of first operation, + 1 to grab the beginning index of the second number
+                    * Second = Grab index of second operation, - index of first operation, - 1 to grab length of second number 
+                    */
+                    numberHoldTwo = double.Parse(str.Substring(op[x].getPoint() + 1, op[x + 1].getPoint() - op[x].getPoint() - 1));
+                    Console.WriteLine("Got second number " + numberHoldTwo.ToString());
+                }
+                else //there are no more points left
+                {
+                    numberHoldTwo = double.Parse(str.Substring(op[x].getPoint() + 1, ((str.Length - 1) - op[x].getPoint())));
+                    Console.WriteLine("Got second number " + numberHoldTwo.ToString());
+                }
+
+                total = doOperation(numberHoldOne, numberHoldTwo, op[x].getOperation());
+                Console.WriteLine("Total so far is {0}", total);
+
+            }
+            Console.WriteLine("Total is {0}", total);
+            tbResult.Text = total.ToString();
+        }
+        #endregion
+
+        #region Calculation Helper
+        /*
+         * Helper method for performCalculation. Takes two numbers and the operation,
+         * performs is and returns it.
+         * */
+        public double doOperation(double x, double y, Operator op)
+        {
+            double t = 0;
+            switch (op)
+            {
+                case Operator.PLUS:
+                    t = x + y;
+                    break;
+                case Operator.MINUS:
+                    t = x - y;
+                    break;
+                case Operator.MULTIPLY:
+                    t = x * y;
+                    break;
+                case Operator.DIVIDE:
+                    t = x / y;
+                    break;
+                default:
+                    break;
+            }
+            return t;
+        }
+    }
+    #endregion
+
+        #region OperationPoint Class
+    public class OperationPoint
+    {
+        private int point;
+        private Operator operation;
+
+        public OperationPoint(int x, Operator op)
+        {
+            setPoint(x);
+            setOperation(op);
+        }
+
+        public void setPoint(int x)
+        {
+            point = x;
+        }
+
+        public void setOperation(Operator op)
+        {
+            operation = op;
+        }
+
+        public int getPoint()
+        {
+            return point;
+        }
+
+        public Operator getOperation()
+        {
+            return operation;
+        }
+    }
+    #endregion
+}
